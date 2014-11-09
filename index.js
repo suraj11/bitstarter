25e48149c6c06a304ee2937e7ad79870002d4cbc
Ubuntu <ubuntu@ip-172-31-20-6.us-west-2.compute.internal>
2014-11-09 23:27:00 +0000

    moded js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express();
+var fs = require('fs');
+var buffer = new Buffer(16);
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-
+fs.readFileSync("index.html", buffer);
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+	response.send(buffer.toString('utf-8'))
+	
 })
 
 app.listen(app.get('port'), function() {

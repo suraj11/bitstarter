596ed1c8eb2b9adb57a1d5db6fee7e806f5c35ae
Ubuntu <ubuntu@ip-172-31-20-6.us-west-2.compute.internal>
2014-11-18 05:21:53 +0000

    aa

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
 	response.sendfile(__dirname + '/index.html')
 })
-app.get('/about', function(request, response) {
-        response.sendfile(__dirname + '/about.html')
-})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))

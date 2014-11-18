fdbe66d5d7eaf9fa0adb41e12f7529fb0c5a9706
Ubuntu <ubuntu@ip-172-31-20-6.us-west-2.compute.internal>
2014-11-18 04:57:08 +0000

    lk

--- app.js
+++ app.js
@@ -6,7 +6,4 @@ app.get('/', function(request, response) {
     response.sendfile(__dirname + '/index.html');
 }).configure(function() {
     app.use('/', express.static(__dirname + '/public'));
-    app.use('/', express.static(__dirname + '/img'));
-    app.use('/', express.static(__dirname + '/js'));
-    app.use('/', express.static(__dirname + '/css'));	
 }).listen(port);

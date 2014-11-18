33016dbbff324101e9b8aac95d7f5e9d02f8d128
Ubuntu <ubuntu@ip-172-31-20-6.us-west-2.compute.internal>
2014-11-18 05:19:58 +0000

    aa

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/img'))
+app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
 	response.sendfile(__dirname + '/index.html')
 })

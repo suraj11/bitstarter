169a0b320088ea4226ced54c0a26267f88e7199c
Ubuntu <ubuntu@ip-172-31-20-6.us-west-2.compute.internal>
2014-11-19 03:47:51 +0000

    hh

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var buffer = new Buffer(16);
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 app.use('/img', express.static(__dirname + '/img'))
+app.use('/js', express.static(__dirname + '/js'))
+app.use('/css', express.static(__dirname + '/css'))
 buffer = fs.readFileSync("index.html");
 app.get('/', function(request, response) {
 	response.send(buffer.toString('utf-8'))
